a1eacf913282b4440b4d8d5985f0f4bbd0380842
al-jabir <aljabir098@gmail.com>
2023-04-13 00:11:52 +0600

    asp.net core project practice

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyProject.Models;
+using System.Diagnostics;
+
+namespace MyProject.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About ()
+        {
+            return View();
+        }
+        public IActionResult Skills()
+        {
+            return View();
+        }
+        public IActionResult Portfolio()
+        {
+            return View();
+           
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
